e59a6d7facd48469663ee1937ddae418d33464c3
Crowdbotics <team@crowdbotics.com>
2021-11-11 15:10:37 +0000

    Update models

--- backend/rebalance_31924/wsgi.py
+++ backend/rebalance_31924/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rebalance_31924.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rebalance_31924.settings")
 
 application = get_wsgi_application()
