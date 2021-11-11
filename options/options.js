b63ca49e8e2248f647a5e78cc328e1613ede302e
Crowdbotics <team@crowdbotics.com>
2021-11-11 15:05:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "rebalance_31924",
+  url: "https://rebalance_31924.botics.co",
+  api: "https://rebalance_31924.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
